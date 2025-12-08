52b47cf3bcc4bbf7582af80823c82aea37cfb72e
Zaksa <zhaksulka@gmail.com>
2025-12-08 18:39:45 +0500

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello World from my Veterinary clinic project!"));
+        IO.println(String.format("Hello from my Veterinary clinic project!"));
 
         IO.println(String.format("           __.                                              \n" +
                 "        .-\".'                      .--.            _..._    \n" +
