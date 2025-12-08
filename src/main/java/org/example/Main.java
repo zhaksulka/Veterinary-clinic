e3da968d71724880aa03ff4a311f3b01b0ef31b0
Zaksa <zhaksulka@gmail.com>
2025-12-08 18:33:37 +0500

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        IO.println(String.format("Hello World from my Veterinary clinic project!"));
+
+        IO.println(String.format("           __.                                              \n" +
+                "        .-\".'                      .--.            _..._    \n" +
+                "      .' .'                     .'    \\       .-\"\"  __ \"\"-. \n" +
+                "     /  /                     .'       : --..:__.-\"\"  \"\"-. \\\n" +
+                "    :  :                     /         ;.d$$    sbp_.-\"\"-:_:\n" +
+                "    ;  :                    : ._       :P .-.   ,\"TP        \n" +
+                "    :   \\                    \\  T--...-; : d$b  :d$b        \n" +
+                "     \\   `.                   \\  `..'    ; $ $  ;$ $        \n" +
+                "      `.   \"-.                 ).        : T$P  :T$P        \n" +
+                "        \\..---^..             /           `-'    `._`._     \n" +
+                "       .'        \"-.       .-\"                     T$$$b    \n" +
+                "      /             \"-._.-\"               ._        '^' ;   \n" +
+                "     :                                    \\.`.         /    \n" +
+                "     ;                                -.   \\`.\"-._.-'-'     \n" +
+                "    :                                 .'\\   \\ \\ \\ \\         \n" +
+                "    ;  ;                             /:  \\   \\ \\ . ;        \n" +
+                "   :   :                            ,  ;  `.  `.;  :        \n" +
+                "   ;    \\        ;                     ;    \"-._:  ;        \n" +
+                "  :      `.      :                     :         \\/         \n" +
+                "  ;       /\"-.    ;                    :                    \n" +
+                " :       /    \"-. :                  : ;                    \n" +
+                " :     .'        T-;                 ; ;        \n" +
+                " ;    :          ; ;                /  :        \n" +
+                " ;    ;          : :              .'    ;       \n" +
+                ":    :            ;:         _..-\"\\     :       \n" +
+                ":     \\           : ;       /      \\     ;      \n" +
+                ";    . '.         '-;      /        ;    :      \n" +
+                ";  \\  ; :           :     :         :    '-.      \n" +
+                "'.._L.:-'           :     ;    bug   ;    . `. \n" +
+                "                     ;    :          :  \\  ; :  \n" +
+                "                     :    '-..       '.._L.:-'  \n" +
+                "                      ;     , `.                \n" +
+                "                      :   \\  ; :                \n" +
+                "                      '..__L.:-'"));
+
+    }
+}
